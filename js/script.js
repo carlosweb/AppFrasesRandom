b86386dfe729f3ff00f10f24be56023bd7bae1de
carlosweb <kaduinfo@gmail.com>
2020-03-23 15:45:07 -0300

    Ajustes no css

--- js/script.js
+++ js/script.js
@@ -43,10 +43,10 @@ function showDb() {
   
             for (let i = 0; i < dados.length; i++){
                 next.innerHTML = dados[i].frases[Math.floor(Math.random()*dados[i].frases.length)]
-                var rand = Math.floor(Math.random()*dados[i].images.length)
-                var links = dados[i].images
-                img.innerHTML = `<img src="${currentLink}${links[rand]}" id="images">`
-                fontColor.style.color = dados[i].color[Math.floor(Math.random()*dados[i].color.length)]
+                // var rand = Math.floor(Math.random()*dados[i].images.length)
+                // var links = dados[i].images
+                // img.innerHTML = `<img src="${currentLink}${links[rand]}" id="images">`
+                // fontColor.style.color = dados[i].color[Math.floor(Math.random()*dados[i].color.length)]
             }
         }
     }
